41ea8cf68332d3f01a241d13f8fa4f5963644ce3
r1oga <r1oga@tuta.io>
2021-03-21 15:15:27 +0100

    Complete exercice 2

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -2,31 +2,27 @@
 // http://localhost:3000/isolated/exercise/02.js
 
 import * as React from 'react'
-import {Switch} from '../switch'
+import { Switch } from '../switch'
 
-function Toggle() {
+function Toggle({ children }) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  // 🐨 replace this with a call to React.Children.map and map each child in
-  // props.children to a clone of that child with the props they need using
-  // React.cloneElement.
-  // 💰 React.Children.map(props.children, child => {/* return child clone here */})
-  // 📜 https://reactjs.org/docs/react-api.html#reactchildren
-  // 📜 https://reactjs.org/docs/react-api.html#cloneelement
-  return <Switch on={on} onClick={toggle} />
+  return React.Children.map(children, child =>
+    allowedTypes.includes(child.type)
+      ? React.cloneElement(child, {
+          on,
+          toggle
+        })
+      : child
+  )
 }
 
-// 🐨 Flesh out each of these components
-
-// Accepts `on` and `children` props and returns `children` if `on` is true
-const ToggleOn = () => null
-
-// Accepts `on` and `children` props and returns `children` if `on` is false
-const ToggleOff = () => null
-
-// Accepts `on` and `toggle` props and returns the <Switch /> with those props.
-const ToggleButton = () => null
+const ToggleOn = ({ on, children }) => (on ? children : null)
+const ToggleOff = ({ on, children }) => (on ? null : children)
+const ToggleButton = ({ on, toggle }) => <Switch on={on} onClick={toggle} />
+const CustomToggle = ({ on }) => (on ? 'YOOOOO' : 'BOOOO')
+const allowedTypes = [ToggleOn, ToggleOff, ToggleButton]
 
 function App() {
   return (
@@ -34,7 +30,9 @@ function App() {
       <Toggle>
         <ToggleOn>The button is on</ToggleOn>
         <ToggleOff>The button is off</ToggleOff>
+        <span>Hello</span>
         <ToggleButton />
+        <CustomToggle />
       </Toggle>
     </div>
   )
